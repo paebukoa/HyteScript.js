0089bd82008f615550f38d3af4106e0ad4c40142
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-01-29 02:06:53 -0300

    fixed comparision symbol

--- errors.js
+++ errors.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require("discord.js");
 
 class SetError {
     constructor(d, type, message) {
-            if(type = "function") {
+            if(type === "function") {
             d.message.channel.send("FunctionError: " + message);
 
             return;
