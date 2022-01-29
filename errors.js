d6ef703ea60ff8389d8031569f4837200dab8e2c
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-01-29 01:53:30 -0300

    It didn't work

--- errors.js
+++ errors.js
@@ -1,19 +1,9 @@
 const { MessageEmbed } = require("discord.js");
 
 class SetError {
-    constructor(channel, type, message) {
+    constructor(d, type, message) {
             if(type = "function") {
-            const embed = new MessageEmbed()
-            .setTitle("Error!")
-            .setDescription(`An error just happenned.
-            
-            Function: \`#${d.func}\`
-            Error: \`${message}\`
-            Command: \`${d.command}\`
-            `)
-            .setColor("RED");
-
-            channel.send({embeds: [embed]});
+            d.message.channel.send("FunctionError: " + message);
 
             return;
         }
