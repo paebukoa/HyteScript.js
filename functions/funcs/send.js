0663f57744782376fb96a7fea72b40c95d527ad8
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-01-29 19:05:22 -0300

    CHANGE

--- functions/funcs/send.js
+++ functions/funcs/send.js
@@ -1,8 +1,6 @@
 module.exports = async d => {
     let [message, channelId = d.message.channel?.id, returnId] = d.inside.splits;
     const channel = d.client.channels.cache.get(channelId);
-    if (!channel) d.error.set.newError(d, "function", `Invalid channel ID: ${channelId}`);
-        
-    channel.send(message);
+    if (!channel) d.error.set.newError(d, "function", `Invalid channel ID: ${channelId}`); else channel.send(message);
     d.result = "";
 }
