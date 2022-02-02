059cd4b4cffc91b12f617ca2d52484019a5bf524
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-02-02 12:02:46 -0300

    Update splitElement.js

--- code/functions/funcs/splitElement.js
+++ code/functions/funcs/splitElement.js
@@ -1,9 +1,9 @@
 module.exports = async d => {
     let [index] = d.inside.splits;
 
-    if(isNaN(index) || index <= 0) {
+    if(isNaN(index) || Number(index) < 1) {
         d.error.set.newError(d, "function", `Invalid index "${index}" provided.`);
         return;
     }
-    d.result = d.split[index - 1];
+    d.result = d.split[Number(index) - 1];
 }
