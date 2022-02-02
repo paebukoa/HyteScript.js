d1aac253296efeedd6307b5c59fdebb922185cce
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-02-02 01:11:17 -0300

    Fixed codeReader.js

--- code/codeReader.js
+++ code/codeReader.js
@@ -76,7 +76,6 @@ class CodeReader {
                     } catch (e) {
                         console.error(e);
                     }
-                    return;
                 }
             });
         });
