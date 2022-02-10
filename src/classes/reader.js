8ca7fe7659258314ed3b1f774f77d3157a977844
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-02-09 22:35:32 -0300

    Added reader code

--- src/classes/reader.js
+++ src/classes/reader.js
@@ -1,9 +1,37 @@
 class reader {
     constructor(data, code) {
-        this.exec = {
-            result: code,
-            error: false
-        }
+        this.exec.result = code;
+
+        let importantFunctions = code.split("<!").slice(1).reverse();
+
+        importantFunctions.map(func => {
+            if (!func.includes(">")) return;
+            
+            let inside = func.split(">")[0];
+            let func = inside.split(" ")[0];
+            let params = inside.split(" ").slice(1).join(" ").trim();
+
+            this.data.err = false;
+            this.data = data;
+            this.data.inside = inside;
+            this.data.func = func;
+            this.data.params = {
+                raw: params,
+                splits: params.split(" ")
+            };
+
+            let foundFunc = this.data.funcs.find(f => f.name.toLowerCase() === func.toLowerCase());
+
+            if (!foundFunc) return;
+
+            foundFunc.run(this.data);
+
+            const replaceLast = this.exec.result.split(`<${inside}>`);
+            const slice = replaceLast.pop();
+            this.exec.result = replaceLast.join(`<${inside}>`) + this.data.result + slice;
+        });
+
+        this.exec.error = this.data.err;
     }
 }
 
