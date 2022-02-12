dc307e069a2ad18f593f533cd55753e8a3640f36
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-02-12 01:03:37 -0300

    functionError bug fixed

--- src/functions/funcs/util/send.js
+++ src/functions/funcs/util/send.js
@@ -2,12 +2,12 @@ module.exports = async d => {
     let [message, embeds = "[]", channelId = d.channel?.id, returnId = "false"] = d.params.splits;
 
     let channel = d.client.channels.cache.get(channelId);
-    if (!channel) return d.errors.functionError(d, `channel ID "${channelId}" is invalid!`);
+    if (!channel) return d.error.functionError(d, `channel ID "${channelId}" is invalid!`);
 
     try {
         embeds = JSON.parse(embeds);
     } catch (e) {
-        return d.errors.functionError(d, `embed creation failed: ${e}`);
+        return d.error.functionError(d, `embed creation failed: ${e}`);
     };
 
     let messageObj = {
