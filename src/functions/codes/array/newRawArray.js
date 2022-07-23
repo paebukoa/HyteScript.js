ea4eec03ae3775092cbb5174e283b03fb01b876a
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-07-23 02:03:52 -0300

    fixed bug with newRawArray

--- src/functions/codes/array/newRawArray.js
+++ src/functions/codes/array/newRawArray.js
@@ -4,8 +4,7 @@ module.exports = async d => {
     if (!array.startsWith("[")) return d.throwError.invalid(d, "array", array);
 
     try {
-        let parsedArray = JSON.parse(array);
-        parsedArray.map(element => typeof element !== 'string' ? JSON.stringify(element) : element)
+        let parsedArray = JSON.parse(array).map(element => typeof element !== 'string' ? JSON.stringify(element) : element);
         d.data.arrays[name] = parsedArray;
     } catch (e) {
         return d.throwError.func(d, `failed to create new raw array: ${e}`);
