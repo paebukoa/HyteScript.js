d9d68f08a1574691c2043053fad24cda6f63c702
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-08-02 17:30:59 -0300

    fixed old method

--- src/functions/codes/info/error.js
+++ src/functions/codes/info/error.js
@@ -1,5 +1,5 @@
 module.exports = async d => {
     let [property = "message"] = d.function.parameters;
     
-    return d.data.errorData[property];
+    return d.data.error[property.toLowerCase()];
 }
