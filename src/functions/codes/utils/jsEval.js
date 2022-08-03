bfd5579d289f31268e5579030bb21042549ac555
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-08-03 10:17:57 -0300

    improved jsEval

--- src/functions/codes/utils/jsEval.js
+++ src/functions/codes/utils/jsEval.js
@@ -1,3 +1,4 @@
 module.exports = async (d, jsCode) => {
-    return JSON.stringify(eval(jsCode), null, 2);
+    let evaled = eval(jsCode)
+    return typeof evaled == 'string' ? evaled : JSON.stringify(evaled, null, 2);
 };
