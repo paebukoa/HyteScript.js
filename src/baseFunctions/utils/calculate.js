99642996279e59ce29a8ed73a6afc570551b9fbb
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-08-17 06:53:50 -0300

    fixed `#(calculate)`

--- src/baseFunctions/utils/calculate.js
+++ src/baseFunctions/utils/calculate.js
@@ -2,6 +2,13 @@ module.exports = async d => {
     let [expression] = d.function.parameters;
 
     if(expression.replaceAll(" ", "") !== expression.replace(/[^-()\d/*+.]/g, '')) return new d.error("invalid", d, 'expression', expression);
+	
+    let evaled;
+	try {
+		evaled = eval(expression.replace(/[^-()\d/*+.]/g, ''))
+	} catch {
+		return new d.error("invalid", d, 'expression', expression);
+	}
 
-    return eval(expression.replace(/[^-()\d/*+.]/g, ''));
+    return evaled;
 };
