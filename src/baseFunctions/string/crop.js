87e1bc2c3b893eb1be2bcf374983b931b1342eb9
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-08-23 00:38:52 -0300

    updated methods

--- src/baseFunctions/string/crop.js
+++ src/baseFunctions/string/crop.js
@@ -1,7 +1,7 @@
 module.exports = async d => {
     let [text, maxLength, suffix = "..."] = d.function.parameters;
 
-    if (isNaN(maxLength) || Number(maxLength) < 1) return d.throwError.default(d, "max length", maxLength);
+    if (isNaN(maxLength) || Number(maxLength) < 1) return new d.error('invalid', d, "max length", maxLength);
 
     let croppedText = text.slice(0, Number(maxLength));
     
