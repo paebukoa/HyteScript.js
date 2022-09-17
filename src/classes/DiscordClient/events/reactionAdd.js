b29f565af9679c145a529fa6fde8627ab4471060
paebukoa <96163397+paebukoa@users.noreply.github.com>
2022-09-17 14:04:27 -0300

    removed random logs

--- src/classes/DiscordClient/events/reactionAdd.js
+++ src/classes/DiscordClient/events/reactionAdd.js
@@ -1,15 +1,11 @@
 const { clone, replaceLast } = require("../utils/utils")
 
 module.exports = async d => {
-
-    console.log('ok')
     let requiredIntents = ['GuildMessages', 'GuildMembers', 'GuildMessageReactions']
 
     if (requiredIntents.find(intent => !d.clientOptions.intents.includes(intent))) new d.error('requiredIntent', replaceLast(__filename.replace("/", "\\").split('\\').at('-1'), '.js', ''), ...requiredIntents)
     
     d.client.on('messageReactionAdd', async (reaction, user) => {
-        console.log('message reacted')
-
         d.commandManager.reactionAdd.forEach(async commandData => {
             let data = clone(d)
     
